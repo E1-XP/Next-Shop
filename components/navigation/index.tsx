1ee28fa80f5195a4c402ee0332220a36f53766b5
E1-XP <poczta.elektroniczna@hotmail.com>
2022-12-31 18:25:46 +0100

    add bootstrap, base navbar structure

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -3,34 +3,66 @@ import Link from "next/link";
 import styles from "./navigation.module.scss";
 
 export const Navigation = () => (
-  <nav className={styles.navigation}>
-    <h1 className={styles.navigation__logo}>
-      <Link href="/">Next-shop</Link>
-    </h1>
-    <ul className={styles.navigation__list}>
-      <li className={styles.navigation__item}>
-        <Link href="#">New arrivals</Link>
-      </li>
-      <li className={styles.navigation__item}>
-        <Link href="#">Men</Link>
-      </li>
-      <li className={styles.navigation__item}>
-        <Link href="#">Women</Link>
-      </li>
-      <li className={styles.navigation__item}>
-        <Link href="#">Kids</Link>
-      </li>
-    </ul>
-    <ul className={styles.navigation__list}>
-      <li className={styles.navigation__item}>
-        <Link href="#">x</Link>
-      </li>
-      <li className={styles.navigation__item}>
-        <Link href="#">x</Link>
-      </li>
-      <li className={styles.navigation__item}>
-        <Link href="#">x</Link>
-      </li>
-    </ul>
+  <nav className={`navbar navbar-expand-lg ${styles.navigation}`}>
+    <div className="container">
+      <h1 className={`navbar-brand ${styles.navigation_logo}`}>
+        <Link href="/">Next-shop</Link>
+      </h1>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className={`collapse navbar-collapse justify-content-between ${styles.navigation__listwrapper}`}
+        id="navbarSupportedContent"
+      >
+        <ul className={`navbar-nav my-2 ${styles.navigation__list}`}>
+          <li className={`nav-item ${styles.navigation__item}`}>
+            <Link className="nav-link" href="#">
+              New arrivals
+            </Link>
+          </li>
+          <li className={`nav-item ${styles.navigation__item}`}>
+            <Link className="nav-link" href="#">
+              Men
+            </Link>
+          </li>
+          <li className={`nav-item ${styles.navigation__item}`}>
+            <Link className="nav-link" href="#">
+              Women
+            </Link>
+          </li>
+          <li className={`nav-item ${styles.navigation__item}`}>
+            <Link className="nav-link" href="#">
+              Kids
+            </Link>
+          </li>
+        </ul>
+        <ul className={`navbar-nav ${styles.navigation__list}`}>
+          <li className={`nav-item ${styles.navigation__item}`}>
+            <Link className="nav-link" href="#">
+              x
+            </Link>
+          </li>
+          <li className={`nav-item ${styles.navigation__item}`}>
+            <Link className="nav-link" href="#">
+              x
+            </Link>
+          </li>
+          <li className={`nav-item ${styles.navigation__item}`}>
+            <Link className="nav-link" href="#">
+              x
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import "../styles/main.scss";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+
+import "../styles/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />;
 }

7af6c99f88628e8c4af464469c3ac4f78a97efee
E1-XP <poczta.elektroniczna@hotmail.com>
2023-01-25 23:09:19 +0100

    make product rating separate component

--- components/product-item/index.tsx
+++ components/product-item/index.tsx
@@ -2,6 +2,7 @@ import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 
 import { Product } from "../../store/interfaces";
+import { ProductRating } from "../product-rating";
 
 import styles from "./product-item.module.scss";
 
@@ -18,28 +19,7 @@ export const ProductCard = ({ product }: Props) => (
     />
     <div className={`card-body ${styles.card__body}`}>
       <p className="card-text">
-        {Array(5)
-          .fill(null)
-          .map((_, i) => (
-            <svg
-              key={i}
-              xmlns="http://www.w3.org/2000/svg"
-              className={`icon icon-tabler icon-tabler-star ${
-                styles.product__rating
-              } ${i < product.rating ? styles["product__rating--active"] : ""}`}
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="grey"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
-            </svg>
-          ))}
+        <ProductRating rating={product.rating} />
       </p>
       <p className="card-text">{product.brand}</p>
       <Link href="#" className="card-text stretched-link">
---
+++ components/product-rating/index.tsx
@@ -0,0 +1,32 @@
+import styles from "./product-rating.module.scss";
+
+interface Props {
+  rating: number;
+}
+
+export const ProductRating = ({ rating }: Props) => (
+  <>
+    {Array(5)
+      .fill(null)
+      .map((_, i) => (
+        <svg
+          key={i}
+          xmlns="http://www.w3.org/2000/svg"
+          className={`icon icon-tabler icon-tabler-star ${
+            styles["product-rating__item"]
+          } ${i < rating ? styles["product-rating__item--active"] : ""}`}
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="grey"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+        </svg>
+      ))}
+  </>
+);

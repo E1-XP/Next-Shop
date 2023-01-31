0ff7706b951ca530d64427e9aac1d276ac000074
E1-XP <poczta.elektroniczna@hotmail.com>
2023-01-31 22:45:32 +0100

    add zoom-in function into product gallery

--- components/product-gallery/index.tsx
+++ components/product-gallery/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { MouseEvent, useCallback, useRef, useState } from "react";
 
 import { Product } from "../../store/interfaces";
 
@@ -9,38 +10,72 @@ interface Props {
   product: Product;
 }
 
-export const ProductGallery = ({ product }: Props) => (
-  <div className={"row " + styles["product-gallery"]}>
-    <div className="col-md-2">
-      {product.images.map((src, idx) => (
-        <Link
-          href="#"
-          key={idx}
-          className={"d-block mb-3 " + styles["product-gallery__thumb-link"]}
-        >
+export const ProductGallery = ({ product }: Props) => {
+  const zoomElemRef = useRef<HTMLDivElement>();
+  const mainImgRef = useRef<HTMLImageElement>();
+
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+  const zoomInOut = useCallback(
+    (e: MouseEvent) => {
+      const rect = mainImgRef.current?.getBoundingClientRect();
+
+      const x = e.pageX - rect?.left;
+      const y = e.pageY - rect?.top;
+      const imgWidth = rect?.width;
+      const imgHeight = rect?.height;
+      const xPercent = (x / imgWidth) * 100;
+      const yPercent = (y / imgHeight) * 100;
+
+      setMousePos({ x: xPercent, y: yPercent });
+    },
+    [mainImgRef]
+  );
+
+  return (
+    <div className={"row " + styles["product-gallery"]}>
+      <div className="col-md-2">
+        {product.images.map((src, idx) => (
+          <Link
+            href="#"
+            key={idx}
+            className={"d-block mb-3 " + styles["product-gallery__thumb-link"]}
+          >
+            <Image
+              className={"img-fluid"}
+              src={src}
+              alt={`model presenting ${product.brand} ${product.name}`}
+            />
+            <div
+              className={
+                styles["product-gallery__thumb-cover"] +
+                " " +
+                (!idx ? styles.active : "")
+              }
+            ></div>
+          </Link>
+        ))}
+      </div>
+      <div className="col-md-10">
+        <Link href="#" className="d-block position-relative">
           <Image
-            className={"img-fluid"}
-            src={src}
+            className={"img-fluid " + styles["product-gallery__main-img"]}
+            src={product.images[0]}
             alt={`model presenting ${product.brand} ${product.name}`}
+            ref={mainImgRef}
           />
           <div
-            className={
-              styles["product-gallery__thumb-cover"] +
-              " " +
-              (!idx ? styles.active : "")
-            }
+            className={styles["product-gallery__zoom"]}
+            style={{
+              background: `url(${product.images[0].src}) no-repeat`,
+              backgroundPositionX: `${mousePos.x}%`,
+              backgroundPositionY: `${mousePos.y}%`,
+            }}
+            onMouseMove={zoomInOut}
+            ref={zoomElemRef}
           ></div>
         </Link>
-      ))}
+      </div>
     </div>
-    <div className="col-md-10">
-      <Link href="#" className="d-block">
-        <Image
-          className="img-fluid"
-          src={product.images[0]}
-          alt={`model presenting ${product.brand} ${product.name}`}
-        />
-      </Link>
-    </div>
-  </div>
-);
+  );
+};
